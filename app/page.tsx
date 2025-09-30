dc1147670b1849b9a18deb35bd0abfda2f0a9caf
Zain <zainhussaini9898@gmail.com>
2025-09-30 21:20:08 +0530

    added afew chyanges2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { ThreeDCardDemo } from "@/components/3Dcard";
 import { LayoutTextFlipDemo } from "@/components/FlipText";
-import Globe from "@/components/Globe";
 import { HeroParallaxDemo } from "@/components/HeroParallax";
 import { InfiniteMovingCardsDemo } from "@/components/InfiniteCard";
 import { NavbarDemo } from "@/components/ResizableNavbar";
@@ -9,7 +8,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<div>
 <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <NavbarDemo/>
         <HeroParallaxDemo/>
@@ -20,9 +18,6 @@ export default function Home() {
         <InfiniteMovingCardsDemo/>
 
     </div>
-    <div className="w-[100vw] h-[200vh]">
-      <Globe/>
-      </div>
-    </div>
+
   );
 }
