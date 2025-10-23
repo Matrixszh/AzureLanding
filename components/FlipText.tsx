19fd336809b6169e3c80f9a42b2e15a32a837c74
Zain <zainhussaini9898@gmail.com>
2025-10-23 20:12:50 +0530

    fixed glitch

--- components/FlipText.tsx
+++ components/FlipText.tsx
@@ -8,7 +8,7 @@ export function LayoutTextFlipDemo() {
       <motion.div className="relative mx-4 my-4 flex flex-col items-center justify-center gap-4 text-center sm:mx-0 sm:mb-0 sm:flex-row">
         <LayoutTextFlip
           text="We Provide "
-          words={["Azure Cloud Migration", "Power BI Solutions", "Data Migration","DevOps Services",".NET Developmen","Consulting Services","Consulting Services",  "Recruiting Services"]}
+          words={["Cloud Migration", "Power BI Solutions", "Data Migration","DevOps Services",".NET Developmen","Consulting Services","Consulting Services",  "Recruiting Services"]}
         />
       </motion.div>
       <p className="mt-4 text-center text-lg text-neutral-600 dark:text-neutral-400">
